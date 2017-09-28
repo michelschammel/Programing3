5319aa1620456f62ed6cd4dadfde6f9e9dcf161b
Bjoerg <Bjoerg.Schmidt@googlemail.com>
2017-09-28 16:00:11 +0200

    Constanten in TutorialController eingefügt Tutorial überarbeitet

--- WissenschaftlicheQuellen/src/quellen/constants/Controller_Constants.java
+++ WissenschaftlicheQuellen/src/quellen/constants/Controller_Constants.java
@@ -1,5 +1,10 @@
 package quellen.constants;
 
+
+/**
+ * Enthaelt Kontanten, die in den Controllern vorkommen
+ * @Author Bjoern Schmidt
+ */
 public class Controller_Constants {
 
 
@@ -96,4 +101,44 @@ public class Controller_Constants {
     public final static String KORRIGIERE_FALSCHE_FELDER = "Bitte korrigieren Sie die flaschen Felder!";
 
 
+
+    //QuellenOverviewController
+    public final static String LEERER_STRING = "";
+
+    public final static String NICHTS_AUSGEWAEHLT = "Nichts ausgeählt";
+    public final static String KEINE_QUELLE_AUSGEWAEHLT = "Keine Quelle ausgewählt";
+    public final static String BITTE_QUELLE_AUS_TABELLE = "Bitte wählen Sie eine Quelle aus der Tabelle!";
+
+    public final static String ERROR = "Error!";
+    public final static String KEINE_SUCHE_MOEGLICH = "Keine Suche möglich";
+    public final static String KEINE_EINGABE_ZUM_SUCHEN_MOEGLICH = "Keine Eingabe zum Suchen vorhanden";
+
+
+
+    //TutorialController
+    public static final int TUT_01 = 1;
+    public static final int TUT_02 = 2;
+    public static final int TUT_03 = 3;
+    public static final int TUT_04 = 4;
+    public static final int TUT_05 = 5;
+    public static final int TUT_06 = 6;
+    public static final int TUT_07 = 7;
+    public static final int TUT_08 = 8;
+    public static final int TUT_09 = 9;
+
+
+    public static final int TUT_ANZAHL_PLUS_EINS = 10;
+
+
+    public static final String PIC_TUT_01 = "../resources/tut01.png";
+    public static final String PIC_TUT_02 = "../resources/tut02.png";
+    public static final String PIC_TUT_03 = "../resources/tut03.png";
+    public static final String PIC_TUT_04 = "../resources/tut04.png";
+    public static final String PIC_TUT_05 = "../resources/tut05.png";
+    public static final String PIC_TUT_06 = "../resources/tut06.png";
+    public static final String PIC_TUT_07 = "../resources/tut07.png";
+    public static final String PIC_TUT_08 = "../resources/tut08.png";
+    public static final String PIC_TUT_09 = "../resources/tut09.png";
+    public static final String PIC_TUT_10 = "../resources/tut10.png";
+    public static final String PIC_TUT = "../resources/tut.png";
 }
