a87592b01a3e6fd3127df4d8e9cf7cd2e59fa492
weedric <cedi.schreiner@web.de>
2017-11-16 15:01:31 +0100

    - Quellenattribute können eingefügt oder geupdated werden. !!Eine Erweiterung einer Quelle wird noch nicht verarbeitet

---
+++ src/main/java/enums/SourceStandardAttributes.java
@@ -0,0 +1,9 @@
+package enums;
+
+/**
+ * Created by Cedric on 16.11.2017.
+ * Contains all standard attributes of a source
+ */
+public enum SourceStandardAttributes {
+    year, author, title
+}
--- src/main/java/utilities/DatabaseStringCreator.java
+++ src/main/java/utilities/DatabaseStringCreator.java
@@ -1,8 +1,13 @@
 package utilities;
 
+import dao.ConnectionKlasse;
+import enums.SourceStandardAttributes;
 import models.interfaces.ObjectTemplateInterface;
 import models.interfaces.SourceInterface;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
@@ -13,7 +18,7 @@ import java.util.List;
 public abstract class DatabaseStringCreator {
     private static final String SOURCE_INTERFACE = "SourceInterface";
 
-    public static Statement insertOrUpdateSource(SourceInterface source){
+    public static void insertOrUpdateSource(SourceInterface source){
 
         List<ObjectTemplateInterface> sourceTemplates = SourceReflection.getTemplate(source);
 
@@ -25,20 +30,43 @@ public abstract class DatabaseStringCreator {
                 isSourceInterfaceExtension = false;
             }
         }
-        try {
+        try (Connection connection = getConnection()){
             //If the id of the source is 0 it has to be inserted, otherwise just update the source
-            if (source.getId() != 0) {
-                String insertSource = String.format("INSERT INTO Source(title, year, author) VALUES (%s, %s, %s)", source.getTitle(), source.getYear(), source.getAuthor());
-
+            ResultSet resultSet;
+            Statement statement = connection.createStatement();
+            if (source.getId() == 0) {
+                String insertSource = String.format("INSERT INTO Source(title, year, author) VALUES ('%s', '%s', '%s')", source.getTitle(), source.getYear(), source.getAuthor());
+                statement.execute(insertSource);
+                Statement getSourceId = connection.createStatement();
+                resultSet = getSourceId.executeQuery("SELECT seq FROM sqlite_sequence WHERE name = 'Source'");
+                resultSet.next();
+                source.setId(resultSet.getInt(1));
+            } else {
+                String updateSource = String.format("UPDATE Source SET author = '%s', year = '%s', title = '%s' WHERE id = %d", source.getAuthor(), source.getYear(), source.getTitle(), source.getId());
+                statement.executeUpdate(updateSource);
             }
 
-
+            //if the source has some extra attributes they have to be inserted/updated too
             if (isSourceInterfaceExtension) {
-                System.out.println("EXTRA");
+                updateSourceExtraAttributes(source);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+    }
+
+    private static void updateSourceExtraAttributes(SourceInterface source) throws SQLException{
+        List<ObjectTemplateInterface> template = SourceReflection.getTemplate(source);
+
+        if (template != null) {
+            for (SourceStandardAttributes attribute : SourceStandardAttributes.values()) {
+                template.removeIf(templateRow -> templateRow.getAttributeName().equals(attribute.name()));
+            }
+        }
+    }
+
+    private static Connection getConnection() {
+        ConnectionKlasse con = new ConnectionKlasse();
+        return con.getConnection();
     }
 }
